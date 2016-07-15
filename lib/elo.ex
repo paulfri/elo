dbd40c2b0c30198ce5d4fb31b0586f71ba966b06
Paul Friedman <paulrfri@gmail.com>
2016-07-14 22:45:16 -0400

    Initial implementation

--- lib/elo.ex
+++ lib/elo.ex
@@ -1,2 +1,44 @@
 defmodule Elo do
+  # TODO: adjustable k-factor
+  @k_factor 25
+
+  @doc """
+  Calculate new Elo ratings for two given existing ratings (`player_rating`
+  and `opponent_rating`) and a given `result`.
+
+  Result must be 0.0 (first rating loses), 0.5 (draw), or 1.0 (first rating wins).
+
+  ## Examples
+
+      iex(1)> Elo.rate(1000, 500, 1.0)
+      {1001.3310053800506, 523.6689946199494}
+  """
+  def rate(player, opponent, result) when result in [0.0, 0.5, 1.0] do
+    {new_rating(player, opponent, result), new_rating(opponent, player, result)}
+  end
+
+  @doc """
+  Calculate the expected result for the given `player` and `opponent` ratings.
+
+  The expected result is expressed as a winning percentage (probability that
+  `player` will win, plus half the probability of a draw).
+  """
+  def expected_result(player, opponent) do
+    1.0 / (1.0 + (:math.pow(10.0, ((opponent - player) / 400.0))))
+  end
+
+  defp new_rating(player, opponent, result) do
+    player
+    |> expected_result(opponent)
+    |> elo_change(result)
+    |> elo_result(player)
+  end
+
+  defp elo_change(expected_result, result) do
+    @k_factor * (result - expected_result)
+  end
+
+  defp elo_result(change, original) do
+    change + original
+  end
 end
--- test/elo_test.exs
+++ test/elo_test.exs
@@ -2,7 +2,27 @@ defmodule EloTest do
   use ExUnit.Case
   doctest Elo
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @player   1000
+  @opponent 900
+
+  test "win produces correct result" do
+    {player, opponent} = Elo.rate(@player, @opponent, 1.0)
+
+    assert player   == 1008.9983750049279
+    assert opponent ==  916.0016249950721
+  end
+
+  test "loss produces correct result" do
+    {player, opponent} = Elo.rate(@player, @opponent, 0.0)
+
+    assert player   == 983.9983750049279
+    assert opponent == 891.0016249950721
+  end
+
+  test "draw produce correct result" do
+    {player, opponent} = Elo.rate(@player, @opponent, 0.5)
+
+    assert player   == 996.4983750049279
+    assert opponent == 903.5016249950721
   end
 end
